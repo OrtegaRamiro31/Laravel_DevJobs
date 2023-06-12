7a5e69348c54e44d20006ced8c017c173ac57b9a
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-11 18:58:16 -0600

    Componente 'link.blade.php' creado para enlaces. Uso del componente anterior en el login.blade.php

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
-</div>
+@php
+    $classes = "underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+@endphp
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{$slot}}
+</a>
