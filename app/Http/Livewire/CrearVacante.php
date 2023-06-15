69daa571869b85c3cd601ae34e88530a7954e972
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-14 18:43:03 -0600

    Redirección y mensaje al crear la vacante exitosamentem, agregada

--- app/Http/Livewire/CrearVacante.php
+++ app/Http/Livewire/CrearVacante.php
@@ -52,8 +52,10 @@ class CrearVacante extends Component
         ]);
 
         // Crear un mensaje
+        session()->flash('mensaje', 'La vacante se publicó correctamente');
 
         // Redireccionar al usuario
+        return redirect()->route('vacantes.index');
     } 
     public function render()
     {
