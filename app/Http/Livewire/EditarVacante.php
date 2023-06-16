29c575c195e2cbdd5aa867480878520772efbe29
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-15 19:39:01 -0600

    Componente de livewire para editar vacante editar-vacante.blade.php agregada. Se agregó contenido al componente anterior.

---
+++ app/Http/Livewire/EditarVacante.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Salario;
+use Livewire\Component;
+use App\Models\Categoria;
+
+class EditarVacante extends Component
+{
+    public function render()
+    {
+        $salarios = Salario::all();
+        $categorias = Categoria::all();
+        return view('livewire.editar-vacante', [
+            'salarios' => $salarios,
+            'categorias' => $categorias,
+        ]);
+    }
+}
