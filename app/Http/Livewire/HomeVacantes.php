25a7f59e1a526b3ec4bd139cffd4497f151d9634
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-16 14:10:48 -0600

    Funcionalidad de búsqueda por titulo, empresa, categoria y salario agregada al buscador.

--- app/Http/Livewire/HomeVacantes.php
+++ app/Http/Livewire/HomeVacantes.php
@@ -26,9 +26,21 @@ class HomeVacantes extends Component
         // $vacantes = Vacante::all();
 
         // Cuando se tenga un término -> ejecuta el query con where que contiene el Like
-        $vacantes = Vacante::when($this->termino, function($query) {
-            $query->where('titulo', 'LIKE', "%" . $this->termino . "%");
-        })->paginate(20);
+        $vacantes = Vacante::
+            when($this->termino, function($query) {
+                $query->where('titulo', 'LIKE', "%" . $this->termino . "%");
+            })
+            ->when($this->termino, function($query) {
+                $query->orWhere('empresa', 'LIKE', "%" . $this->termino . "%");
+            })
+            ->when($this->categoria, function($query) {
+                $query->where('categoria_id', $this->categoria);
+            })
+            ->when($this->salario, function($query) {
+                $query->where('salario_id', $this->salario);
+            })
+            ->paginate(20);
+        
 
         return view('livewire.home-vacantes', [
             'vacantes' => $vacantes,
