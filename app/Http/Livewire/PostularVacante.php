7dbd6412f0b7c0114c09fc53680afe077eb83f36
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-16 09:29:26 -0600

    Validación para que el input del formulario solo solo acepte PDF, agregada.

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -6,6 +6,25 @@ use Livewire\Component;
 
 class PostularVacante extends Component
 {
+    public $cv;
+
+    protected $rules = [
+        'cv' => 'required|mimes:pdf',
+    ];
+
+    public function postularme()
+    {
+        $this->validate();
+
+        // Almacenar CV en el disco duro
+
+        // Crear la vacante
+
+        // Crear notificación y enviar el email
+
+        // Mostrar al usuario un mensaje de envío correcto
+        
+    }
     public function render()
     {
         return view('livewire.postular-vacante');
