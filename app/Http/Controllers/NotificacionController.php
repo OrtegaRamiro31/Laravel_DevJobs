77f0738c163b8965c35c02270d9cb0d39d30ca68
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-06-16 11:28:35 -0600

    Agregado index.blade en notificaciones para mostrar las notificaciones que reciben los reclutadores.

--- app/Http/Controllers/NotificacionController.php
+++ app/Http/Controllers/NotificacionController.php
@@ -11,7 +11,10 @@ class NotificacionController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
-        dd('Desde notificacionController');
+        $notificaciones = auth()->user()->unreadNotifications;
+
+        return view('notificaciones.index', [
+            'notificaciones' => $notificaciones
+        ]);
     }
 }
